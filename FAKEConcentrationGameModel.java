53464893a55f505d5da3236811bea695ad9a1480
iqrah-tauhid <iqrah.tauhid@hotmail.com>
2016-04-13 09:00:37 -0400

    Added files via upload

--- FAKEConcentrationGameModel.java
+++ FAKEConcentrationGameModel.java
@@ -6,57 +6,44 @@ import java.awt.event.*;
 class FAKEConcentrationGameModel extends GameModel{
    private ImageIcon [] images = new ImageIcon[16];
    final int SIZE = 4;
-   //int winningPick = 
+   int winningPick = 0;
+   int winningPick2 = 0;
    int numberTurnedOver = 0;
    boolean winner = false;
    boolean gameOver = false;
+   String imageName;
+   String imageName2;
+   int win = 0;
    
    FAKEConcentrationGameModel(){
-   
-   
-   
-      for( int i=0;i<images.length;i++){
-       String imageName = images[i].getDescription(); 
-      if(imageName.equals("vacation.jpg"))
-       winningPick = i; // index of Valuable Prize (not goat)
-      }
-   
-      //setting images
-      /* The ArrayList is a list of numbers (names of image files) and sets into images?  */
-      //comment out to debug
-      /*ArrayList<ImageIcon> imageList = new ArrayList<ImageIcon>(Arrays.asList(images));
-      Collections.shuffle(imageList);
-
-      for( int i=0;i<imageList.size();i++)
-         images[i] = imageList.get(i);*/
-         
-      
-         
       for (int t=0; t<8; t++){
          images[t]=new ImageIcon("goat3.jpg");
          }
       for (int t=8; t<16; t++){
          images[t]=new ImageIcon("vacation.jpg");
          }
-      
+   }
 
-}
       public void takeTurn(int t){} 
       
-      //stuff DuhShurlzz Added while HomeGurl Iqrahzzle was in class
-      public static imagedescription(){
-      
-      for(int i=0;i<images.length;i++){
-            String imageName = images[i].getDescription();
-            String imageName2 = images[i].getDescription();
-            if(imageName = imageName2){
-               winningPick = i;
-               winningPick2 = i;
-               win++;
-            return (winningPick, winningPick2);
-      }       
-      
-      //DuhShurlzz Only Added up to this pointz, but it aint rlly work yuh sea
+      //stuff Iqrah tried to fix but fucking failed at
+      static String imagedescription(,String imageName){
+         for(int i=0;i<images.length;i++){
+               String imageName = images[i].getDescription();       
+         }
+      return(imageName);
+      }
+      public static imagedescription2(){
+         for(int i=0;i<images.length;i++){
+               String imageName2 = images[i].getDescription();
+         }
+      return (imageName2);
+      }
+      if(imageName == imageName2){
+         winningPick = i;
+         winningPick2 = i;
+         win++;
+      }  
       
       boolean gameOverStatus(){
       return(false);
@@ -74,7 +61,8 @@ class FAKEConcentrationGameModel extends GameModel{
       return(SIZE);
       } 
       
-      void display(){};      
+      void display(){};     
+       
       String reportWinner(){
       return("You're a whiner. You suck. Congrats!");
       }  
